4816e77de68147b7fcd6d83f3b1bce010160fb23
NicholasETerry <nicholas.e.terry@gmail.com>
2021-02-24 12:48:37 -0600

    constructed a game_collection list to hold get requests. Loop through and display on html page

--- datatracker/blue_prints/videogame_blueprint.py
+++ datatracker/blue_prints/videogame_blueprint.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, redirect, flash, render_template, url_for, Blueprint
+import requests, json
+from types import SimpleNamespace
 
 
 bp = Blueprint('videogame_blueprint', __name__)
@@ -6,7 +8,16 @@ bp = Blueprint('videogame_blueprint', __name__)
 
 @bp.route('/home')
 def test():
-    return "All good on the Home Front"
+    game_collection = [] # create empty list
+    response = requests.get('https://api.dccresource.com/api/games')
+    games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
+
+    for game in games:
+        if game.platform == "Wii" and game.year == 2007:
+            game_collection.append(game) # add game to list if platform is Wii and year 2007
+
+
+    return str(game_collection) # return the collection of games
 
 
 @bp.route('/invest') # see a data visualization of which video game console is best to invest in based on the number
@@ -14,9 +25,11 @@ def test():
 def invest():
     return " Time to invest"
 
+
 @bp.route('/search') # be able to search for a game and see its details
 def search():
     return " search for a game"
+
 
 @bp.route('/custom') # custom search question
 def custom_search():
