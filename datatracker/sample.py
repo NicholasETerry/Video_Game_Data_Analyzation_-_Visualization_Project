2618e77ced53bb73cd15a466c010604b96570726
Penson, Kelly <pensonk86@gmail.com>
2021-02-25 09:14:26 -0800

    edited graph

--- datatracker/sample.py
+++ datatracker/sample.py
@@ -15,6 +15,16 @@ def index():
     return render_template('sample/index.html', message=message, word=phrase)
 
 
+@app.route('/newest')
+def chart():
+labels: list[str] = ["Xbox","Playstation","wii","Playstation 2","Game cube","Nintendo 64","Saga Genesis","Atari"]
+values = [10,9,8,7,6,4,7,8]
+return render_template('chart.html', values=values, labels=labels)
+
+if __name__ == "__main__":
+app.run(host='0.0.0.0', port=5001)
+
+
 @bp.route('/postform', methods=('GET', 'POST'))
 def other_example():
     if request.method == 'POST':
