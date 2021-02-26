7920dddab98bfd05347c9e2d5478d3411341d46d
Penson, Kelly <pensonk86@gmail.com>
2021-02-26 06:24:37 -0800

    edited graph 3

--- datatracker/app.py
+++ datatracker/app.py
@@ -9,7 +9,10 @@ bp = Blueprint('app', __name__)
 
 @bp.route('/newest')
 def chart():
-    labels: list[str] = ["Xbox","Playstation","wii","Playstation 2","Game cube","Nintendo 64","Saga Genesis","Atari"]
-    values = [10,9,8,7,6,4,7,8]
+    labels: list[str] = ["Xbox", "Playstation", "wii", "Playstation 2", "Game cube", "Nintendo 64", "Saga Genesis", "Atari"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
     return render_template('chart.html', values=values, labels=labels)
 
+@bp.route('new')
+def register_blueprint(bp):
+    return
