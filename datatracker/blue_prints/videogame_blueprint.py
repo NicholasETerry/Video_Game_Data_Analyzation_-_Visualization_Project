d7f5694cf5d1888e64ce364e555b928263433488
NicholasETerry <nicholas.e.terry@gmail.com>
2021-02-27 19:35:00 -0600

    create functionalitiy for returning attributes of a game that the user chooses to search for.

--- datatracker/blue_prints/videogame_blueprint.py
+++ datatracker/blue_prints/videogame_blueprint.py
@@ -3,38 +3,40 @@ import requests, json
 from types import SimpleNamespace
 from datatracker.blue_prints.invest_in import Invest
 from datatracker.blue_prints.search import Search
+from datatracker.blue_prints.details import Details
 bp = Blueprint('videogame_blueprint', __name__)
 
-response = requests.get('https://api.dccresource.com/api/games')
+response = requests.get('https://api.dccresource.com/api/games') # if i have time, create a class to call this and below
 games = json.loads(response.content, object_hook=lambda d: SimpleNamespace(**d))
 invest = Invest.invest_results(games)  # will not let me iterate over this
 investing = invest                     # but I can iterate over this
 new_search = Search.search_results(games)  # same as above, can't iterate over this
 searching = new_search                     # but I can iterate over this one
+usable = Details.usable_games(games)    # ''
+game_collection = usable                # ''
 
 
 @bp.route('/home')
 def test():
-    game_collection = []
-    platform_collection = []
-    global_sales = [get_sales("PS3"), get_sales("X360"), get_sales("3DS"), get_sales("PS4"), get_sales("XOne"),
-                    get_sales("WiiU"), get_sales("Wii"), get_sales("PC"), get_sales("PSV"), get_sales("DS"),
-                    get_sales("PSP")]
-
-    # testg = len(games)  # test data - all games - returns 16598
-    # testgc = len(game_collection)  # test data - games that have years -  returns 16327
-    # testi = len(Invest.no_year(games)) # test data - prints to console all games that year is null - returns len 271
+    title = "Video Game Data"
+    plt_col = []
 
     for game in investing:  # this is appending only unique platforms to platform_collection list
-        if game.platform not in platform_collection:
-            platform_collection.append(game.platform)
+        if game.platform not in plt_col:
+            plt_col.append(game.platform)
 
-    # print(platform_collection)  # for testing only. Will print consoles in platform_collection
+    global_sales = [get_sales(plt_col[0]), get_sales(plt_col[1]), get_sales(plt_col[2]), get_sales(plt_col[3]),
+                    get_sales(plt_col[4]), get_sales(plt_col[5]), get_sales(plt_col[6]), get_sales(plt_col[7]),
+                    get_sales(plt_col[8]), get_sales(plt_col[9]), get_sales(plt_col[10])]
+
+    # test_g = len(games)  # test data - all games - returns 16598
+    # test_gc = len(game_collection)  # test data - games that have years -  returns 16327
+    # test_i = len(Invest.no_year(games)) # test data - prints to console all games that year is null - returns len 271
+    # print(plt_col)  # for testing only. Will print consoles in platform_collection
     # test_tup = search_by_game_name("Super Mario Bros.")  # for testing only
     # print(tuple(test_tup))  # for testing only - returns (('NES', 40.24), ('GB', 5.07))
-    return render_template('sample/index.html', x=platform_collection, y=global_sales)
+    return render_template('sample/index.html', x=plt_col, y=global_sales, chart_title=title)
     # 'sample/index.html refers to the folder then the .html'
-
 
 
 def get_sales(platform):
@@ -46,7 +48,7 @@ def get_sales(platform):
 
 
 @bp.route('/invest')  # see a data visualization of which video game console is best to invest in based on the number
-                      # of game copies sold globally on that console since 2013
+# of game copies sold globally on that console since 2013
 def invest():
     return " Time to invest"
 
@@ -56,7 +58,6 @@ def search_by_game_name():
 
     platform_list = []
     sales_list = []
-
     if request.method == 'POST':
         game_name = request.form['name']
         error = None
@@ -65,24 +66,45 @@ def search_by_game_name():
                 platform_list.append(game.platform)
                 sales_list.append(game.globalSales)
 
+        title = game_name + ": Global Sales / Platform"
         if not game_name:
             error = 'You must enter a title'
 
         if error is not None:
             flash(error)
-        elif request.form['name'] == "go home":
-            return redirect(url_for('sample.index'))
         else:
-            return render_template('sample/index.html', x=platform_list, y=sales_list)
+            return render_template('sample/index.html', x=platform_list, y=sales_list, chart_title=title)
 
     else:
         return render_template('sample/index.html', page_title="PostForm from Module Function")
 
-
     # zipped_list = zip(platform_list, sales_list)  # creates a tuple of platform_list and sales_list
-
     #  return render_template('sample/index.html', )
     # 'sample/index.html refers to the folder then the .html'
+
+
+@bp.route('/search/details', methods=("GET", "POST"))
+def get_details_of_game():
+    game_return = []
+    if request.method == 'POST':
+        game_detail = request.form['game']
+        error = None
+        for game in game_collection:
+            if game.name == game_detail:
+                print(game)
+                game_return.append(game)  # returns the attributes of a game but needs cleaning up
+
+        title = game_detail + ": Global Sales / Platform"
+        if not game_detail:
+            error = 'You must enter a title'
+
+        if error is not None:
+            flash(error)
+        else:
+            return render_template('sample/index.html', game=game_detail, game_return=game_return)
+
+    else:
+        return render_template('sample/index.html', page_title="PostForm from Module Function")
 
 
 @bp.route('/custom')  # custom search question
